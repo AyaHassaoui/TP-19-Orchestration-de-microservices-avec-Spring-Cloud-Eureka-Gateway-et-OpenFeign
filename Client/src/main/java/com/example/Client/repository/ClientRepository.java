20b66d1d61883ba0b0846519b8f2e6577a65d238
AyaHassaoui <a.hassaoui9598@uca.ac.ma>
2025-12-01 21:05:09 +0100

    first commit

---
+++ Client/src/main/java/com/example/Client/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.example.Client.repository;
+import com.example.Client.entite.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> { }
